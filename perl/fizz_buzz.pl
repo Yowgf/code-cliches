c0e07407ea03006b341e317745a1f55c62233657
yowgf <alexthomasmol@gmail.com>
2023-11-19 16:30:12 -0600

    Add perl program for fizz buzz

---
+++ perl/fizz_buzz.pl
@@ -0,0 +1,23 @@
+use warnings;
+use strict;
+
+my $n = 100;
+if ($n == 0) {
+  exit();
+}
+my @a = (1..$n);
+for (@a) {
+  my $i = $_;
+  my $output = "";
+  if ($i % 3 == 0) {
+    $output = "${output}Fizz";
+  }
+  if ($i % 5 == 0) {
+    $output = "${output}Buzz";
+  }
+  if ($output eq "") {
+    $output = "$i";
+  }
+  $output = "${output}\n";
+  print($output);
+}
