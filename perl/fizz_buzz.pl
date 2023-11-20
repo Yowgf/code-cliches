2150f2fdbc3c3e7d2f2bc10633ac11cdce5dfed4
yowgf <alexthomasmol@gmail.com>
2023-11-19 19:19:02 -0600

    Identation

--- perl/fizz_buzz.pl
+++ perl/fizz_buzz.pl
@@ -3,21 +3,21 @@ use strict;
 
 my $n = 100;
 if ($n == 0) {
-  exit();
+    exit();
 }
 my @a = (1..$n);
 for (@a) {
-  my $i = $_;
-  my $output = "";
-  if ($i % 3 == 0) {
-    $output = "${output}Fizz";
-  }
-  if ($i % 5 == 0) {
-    $output = "${output}Buzz";
-  }
-  if ($output eq "") {
-    $output = "$i";
-  }
-  $output = "${output}\n";
-  print($output);
+    my $i = $_;
+    my $output = "";
+    if ($i % 3 == 0) {
+        $output = "${output}Fizz";
+    }
+    if ($i % 5 == 0) {
+        $output = "${output}Buzz";
+    }
+    if ($output eq "") {
+        $output = "$i";
+    }
+    $output = "${output}\n";
+    print($output);
 }
