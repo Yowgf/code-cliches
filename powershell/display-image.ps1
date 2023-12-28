b54b102bf1dacd0054ec8bdb63fd50329e043a2d
yowgf <alexthomasmol@gmail.com>
2023-12-28 14:17:55 -0600

    Add display-image PS script

---
+++ powershell/display-image.ps1
@@ -0,0 +1,24 @@
+# Based on https://gist.github.com/zippy1981/969855
+
+# Loosely based on http://www.vistax64.com/powershell/202216-display-image-powershell.html
+
+[void][reflection.assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$file = (Resolve-Path $args)
+
+$img = [System.Drawing.Image]::Fromfile($file);
+
+# This tip from http://stackoverflow.com/questions/3358372/windows-forms-look-different-in-powershell-and-powershell-ise-why/3359274#3359274
+[System.Windows.Forms.Application]::EnableVisualStyles();
+$form = new-object Windows.Forms.Form
+$form.Text = "Image Viewer"
+$form.Width = $img.Size.Width;
+$form.Height =  $img.Size.Height;
+$pictureBox = new-object Windows.Forms.PictureBox
+$pictureBox.Width =  $img.Size.Width;
+$pictureBox.Height =  $img.Size.Height;
+
+$pictureBox.Image = $img;
+$form.controls.add($pictureBox)
+$form.Add_Shown( { $form.Activate() } )
+$form.ShowDialog()
